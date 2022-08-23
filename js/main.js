be18fba10cd4fe653e7d0578c25a0e9623654135
Abir Das <abirdas422@gmail.com>
2022-08-23 20:05:49 +0600

    Handle negative input and add comment

--- js/main.js
+++ js/main.js
@@ -1,66 +1,80 @@
+
+// Return amount in float of input field and inner text
 function amountInFloat(amountId, type) {
     let amountString;
-    if (type === 'value') {
+    if (type === "value") {
         amountString = document.getElementById(amountId).value;
-    } else if (type === 'text') {
+    } else if (type === "text") {
         amountString = document.getElementById(amountId).innerText;
     }
 
-    if (amountString === '' || isNaN(amountString)) {
-        return 'NaN';
+    if (amountString === "" || isNaN(amountString)) {
+        return "NaN";
     } else {
         return parseFloat(amountString);
     }
 }
 
+// Return length of li tag
 function getLengthOfLi() {
     return document.getElementsByTagName("li").length;
 }
 
 const elements = document.getElementsByClassName("select-player");
-const selectedlists = document.getElementById('selected-players');
+const selectedlists = document.getElementById("selected-players");
+
 
 for (const element of elements) {
-    element.addEventListener('click', function (event) {
+    element.addEventListener("click", function (event) {
         const attribute = event.target.parentNode.parentNode.childNodes[1].innerText;
         if (getLengthOfLi() < 5) {
-            const li = document.createElement('li');
+            const li = document.createElement("li");
             li.innerText = attribute;
             selectedlists.append(li);
-            event.target.setAttribute('disabled', '');
+            event.target.setAttribute("disabled", "");
         } else {
             alert("Not More Then 5");
         }
     });
 }
 
-document.getElementById('calculate').addEventListener('click', function () {
-    const playerBudget = amountInFloat('player-budget', 'value');
+// Event for click calculate button
+document.getElementById("calculate").addEventListener("click", function () {
+    const playerBudget = amountInFloat("player-budget", "value");
 
-    if (playerBudget === 'NaN') {
-        alert('Not a valid input');
+    if (playerBudget === "NaN") {
+        alert("Not a valid input");
     } else {
-        const lengthOfLi = getLengthOfLi();
-        if (lengthOfLi === 0) {
-            alert('No player selected');
+        if (playerBudget < 0) {
+            alert("Money can't be negative");
         } else {
-            const totalPlayerExpenses = playerBudget * lengthOfLi;
-            document.getElementById('player-expenses').innerText = totalPlayerExpenses;
+            const lengthOfLi = getLengthOfLi();
+            if (lengthOfLi === 0) {
+                alert("No player selected");
+            } else {
+                const totalPlayerExpenses = playerBudget * lengthOfLi;
+                document.getElementById("player-expenses").innerText = totalPlayerExpenses;
+            }
         }
-
     }
 });
 
-document.getElementById('calculate-total').addEventListener('click', function () {
-    const playerExpenses = amountInFloat('player-expenses', 'text');
-    const managerBudget = amountInFloat('manager-budget', 'value');
-    const coachBudget = amountInFloat('coach-budget', 'value');
+// Event for click calculate total button
+document.getElementById("calculate-total").addEventListener("click", function () {
+    const playerExpenses = amountInFloat("player-expenses", "text");
+    const managerBudget = amountInFloat("manager-budget", "value");
+    const coachBudget = amountInFloat("coach-budget", "value");
 
-    if (playerExpenses !== 'NaN' && managerBudget !== 'NaN' && coachBudget !== 'NaN') {
-        const totalExpenses = playerExpenses + managerBudget + coachBudget;
-        document.getElementById('total-expenses').innerText = totalExpenses;
+    if (playerExpenses !== "NaN" && managerBudget !== "NaN" && coachBudget !== "NaN") {
+        if (playerExpenses >= 0 && managerBudget >= 0 && coachBudget >= 0) {
+            const totalExpenses = playerExpenses + managerBudget + coachBudget;
+            document.getElementById("total-expenses").innerText = totalExpenses;
+        } else {
+            alert("Money can't be negative");
+        }
+
     } else {
-        alert('Not a valid input');
+        alert("Not a valid input");
     }
 
 });
